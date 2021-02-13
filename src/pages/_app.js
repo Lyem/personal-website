a71673c3836fafeefb46e436b81071f5a204b20a
lyem <rfael403@gmail.com>
2021-02-12 22:59:33 -0300

    feat(theme): color theme
    added color theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,14 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme
 
 export default function App({ Component, pageProps }) {
   return (
